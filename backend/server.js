13fcc5f06ced1c32256e7305cf4ef6b39c1f8983
Shikhar2702 <agrawalshikhar27@gmail.com>
2023-08-02 01:15:10 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -9,19 +9,37 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is Running Successfully");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is Running Successfully");
+// });
 // app.get("/api/chats", (req, res) => {
 //   res.send(chats);
 // });
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ----------Deployement---------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// ----------Deployement---------
+
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
