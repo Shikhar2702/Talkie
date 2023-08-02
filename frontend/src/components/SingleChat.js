063b68d6186e18010b0c66680085fbe37785ae7b
Shikhar2702 <agrawalshikhar27@gmail.com>
2023-08-02 18:15:38 +0530

    Dir issue

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -14,7 +14,7 @@ import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
 import { useColorMode } from "@chakra-ui/react";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://talkie-tfic.onrender.com"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
